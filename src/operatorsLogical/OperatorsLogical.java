cbecc168a328a85ad3798b5b74e8e12858b0c6ca
xMAXSIMKAx <maxymram@gmail.com>
2023-11-02 18:44:58 +0100

    Initial commit

---
+++ src/operatorsLogical/OperatorsLogical.java
@@ -0,0 +1,34 @@
+package operatorsLogical;
+
+import java.util.Scanner;
+
+public class OperatorsLogical {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the amount of your income\n");
+        double income = scanner.nextDouble();
+        if (income <= 10000) {
+            double percentageLittle = 2.5;
+            double calculatedPercentage = (income * percentageLittle) / 100;
+            double netIncome = income - calculatedPercentage;
+            System.out.println("\nyour net income is " + netIncome + "including tax 2.5%");
+
+        } else if (income >= 10000 && income <= 25000) {
+            double percentageLittle = 4.3;
+            double calculatedPercentage = (income * percentageLittle) / 100;
+            double netIncome = income - calculatedPercentage;
+            System.out.println("\nyour net income is " + netIncome + " including tax 4.3%");
+
+        } else if (income >= 25000) {
+            double percentageLittle = 6.7;
+            double calculatedPercentage = (income * percentageLittle) / 100;
+            double netIncome = income - calculatedPercentage;
+            System.out.println("\nyour net income is " + netIncome + " including tax 6.7%");
+
+        } else
+            System.out.println("Error.... Try again");
+
+    }
+}
