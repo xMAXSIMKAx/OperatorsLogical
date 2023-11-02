5756baa806a6ee06565b841b3ecd7b299b2f90bf
xMAXSIMKAx <maxymram@gmail.com>
2023-11-02 22:44:24 +0100

    Transfer of variables

--- src/operatorsLogical/OperatorsLogical.java
+++ src/operatorsLogical/OperatorsLogical.java
@@ -3,25 +3,27 @@ package operatorsLogical;
 import java.util.Scanner;
 
 public class OperatorsLogical {
-
+    static double percentageLittle;
+    static double income;
+    static double calculatedPercentage;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the amount of your income\n");
-        double income = scanner.nextDouble();
+        income = scanner.nextDouble();
         if (income <= 10000) {
-            double percentageLittle = 2.5;
-            double calculatedPercentage = income * percentageLittle / 100;
+            percentageLittle = 2.5;
+            calculatedPercentage = income * percentageLittle / 100;
             System.out.println("\nyour net income is " + calculatedPercentage + " including tax 2.5%");
 
         } else if (income > 10000 && income <= 25000) {
-            double percentageLittle = 4.3;
-            double calculatedPercentage = income * percentageLittle / 100;
+            percentageLittle = 4.3;
+            calculatedPercentage = income * percentageLittle / 100;
             System.out.println("\nyour net income is " + calculatedPercentage + " including tax 4.3%");
 
         } else if (income >= 25000) {
-            double percentageLittle = 6.7;
-            double calculatedPercentage = income * percentageLittle / 100;
+            percentageLittle = 4.3;
+            calculatedPercentage = income * percentageLittle / 100;
             System.out.println("\nyour net income is " + calculatedPercentage + " including tax 6.7%");
 
         } else
