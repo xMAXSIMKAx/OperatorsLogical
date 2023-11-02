4acbc93f815f2280647caf386e9517096d8580b9
xMAXSIMKAx <maxymram@gmail.com>
2023-11-02 22:39:28 +0100

    Delete method

--- src/operatorsLogical/OperatorsLogical.java
+++ src/operatorsLogical/OperatorsLogical.java
@@ -11,21 +11,18 @@ public class OperatorsLogical {
         double income = scanner.nextDouble();
         if (income <= 10000) {
             double percentageLittle = 2.5;
-            double calculatedPercentage = (income * percentageLittle) / 100;
-            double netIncome = income - calculatedPercentage;
-            System.out.println("\nyour net income is " + netIncome + "including tax 2.5%");
+            double calculatedPercentage = income * percentageLittle / 100;
+            System.out.println("\nyour net income is " + calculatedPercentage + " including tax 2.5%");
 
-        } else if (income >= 10000 && income <= 25000) {
+        } else if (income > 10000 && income <= 25000) {
             double percentageLittle = 4.3;
-            double calculatedPercentage = (income * percentageLittle) / 100;
-            double netIncome = income - calculatedPercentage;
-            System.out.println("\nyour net income is " + netIncome + " including tax 4.3%");
+            double calculatedPercentage = income * percentageLittle / 100;
+            System.out.println("\nyour net income is " + calculatedPercentage + " including tax 4.3%");
 
         } else if (income >= 25000) {
             double percentageLittle = 6.7;
-            double calculatedPercentage = (income * percentageLittle) / 100;
-            double netIncome = income - calculatedPercentage;
-            System.out.println("\nyour net income is " + netIncome + " including tax 6.7%");
+            double calculatedPercentage = income * percentageLittle / 100;
+            System.out.println("\nyour net income is " + calculatedPercentage + " including tax 6.7%");
 
         } else
             System.out.println("Error.... Try again");
